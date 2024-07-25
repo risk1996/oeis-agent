2bde8e6b9218e41d9bd3e6753d9c4efa69959a88
William Darian <williamdariansutedjo@gmail.com>
2024-07-25 16:54:05 +0900

    refactor: api using async

---
+++ OEIS Agent Tests/API/OEISClientTest.swift
@@ -0,0 +1,4 @@
+//Created for OEIS Agent in 2024
+// Using Swift 5.0
+
+import Foundation
--- OEIS Agent/API/OEISClient.swift
+++ OEIS Agent/API/OEISClient.swift
@@ -1,23 +1,22 @@
 import Foundation
-import Combine
 import Alamofire
 
 struct OEISJsonResponse<T: Codable>: Codable {
   var results: [T]
 }
 
-func search(q: String) -> Future<OEISJsonResponse<Sequence>, AFError> {
+func search(q: String) async throws -> OEISJsonResponse<Sequence> {
   let parameters = [
     "q": [q],
     "fmt": ["json"],
   ]
   
-  return Future<OEISJsonResponse<Sequence>, AFError> { future in
+  return try await withCheckedThrowingContinuation { continuation in
     AF.request("https://oeis.org/search", parameters: parameters)
       .validate()
       .validate(contentType: ["application/json"])
       .responseDecodable(of: OEISJsonResponse<Sequence>.self) { response in
-        future(response.result)
+        continuation.resume(with: response.result)
       }
   }
 }
