ee513ed116d796ea729cff8524899c7c22a75013
William Darian <williamdariansutedjo@gmail.com>
2024-07-25 14:08:00 +0900

    chore: inital project setup

--- OEIS Agent/Item.swift
+++ OEIS Agent/Item.swift
@@ -1,18 +1,11 @@
-//
-//  Item.swift
-//  OEIS Agent
-//
-//  Created by William Darian on 2024/07/25.
-//
-
 import Foundation
 import SwiftData
 
 @Model
 final class Item {
-    var timestamp: Date
-    
-    init(timestamp: Date) {
-        self.timestamp = timestamp
-    }
+  var timestamp: Date
+  
+  init(timestamp: Date) {
+    self.timestamp = timestamp
+  }
 }
--- OEIS Agent/OEIS_AgentApp.swift
+++ OEIS Agent/OEIS_AgentApp.swift
@@ -1,32 +1,25 @@
-//
-//  OEIS_AgentApp.swift
-//  OEIS Agent
-//
-//  Created by William Darian on 2024/07/25.
-//
-
 import SwiftUI
 import SwiftData
 
 @main
 struct OEIS_AgentApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
+  var sharedModelContainer: ModelContainer = {
+    let schema = Schema([
+      Item.self,
+    ])
+    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+    
+    do {
+      return try ModelContainer(for: schema, configurations: [modelConfiguration])
+    } catch {
+      fatalError("Could not create ModelContainer: \(error)")
     }
+  }()
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+    .modelContainer(sharedModelContainer)
+  }
 }
