a66868b98f99f0566b6e96c3eb4def12a813d063
William Darian <williamdariansutedjo@gmail.com>
2024-07-25 15:51:56 +0900

    refactor: api using future

--- OEIS Agent/API/OEISClient.swift
+++ OEIS Agent/API/OEISClient.swift
@@ -1,26 +1,23 @@
 import Foundation
+import Combine
 import Alamofire
 
 struct OEISJsonResponse<T: Codable>: Codable {
   var results: [T]
 }
 
-func search(q: String, onSuccess: @escaping ([Sequence]) -> Void, onFailure: @escaping (Error) -> Void) {
+func search(q: String) -> Future<OEISJsonResponse<Sequence>, AFError> {
   let parameters = [
     "q": [q],
     "fmt": ["json"],
   ]
-
-  AF.request("https://oeis.org/search", parameters: parameters)
-    .validate()
-    .validate(contentType: ["application/json"])
-    .responseDecodable(of: OEISJsonResponse<Sequence>.self) { response in
-      switch response.result {
-      case let .success(data):
-        onSuccess(data.results)
-      case let .failure(error):
-        onFailure(error)
-      }
-    }
   
+  return Future<OEISJsonResponse<Sequence>, AFError> { future in
+    AF.request("https://oeis.org/search", parameters: parameters)
+      .validate()
+      .validate(contentType: ["application/json"])
+      .responseDecodable(of: OEISJsonResponse<Sequence>.self) { response in
+        future(response.result)
+      }
+  }
 }
