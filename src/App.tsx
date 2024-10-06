b3d1681baf30142561cb7c1a5cc6919478acea78
William Darian <williamdariansutedjo@gmail.com>
2024-10-06 13:07:12 +0900

    feat: lazy routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { createMutationObserver } from "@solid-primitives/mutation-observer";
 import { Route, Router } from "@solidjs/router";
 import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import { SolidQueryDevtools } from "@tanstack/solid-query-devtools";
-import type { Component } from "solid-js";
+import { type Component, lazy } from "solid-js";
 
 import Layout from "./components/layout";
 import { observeBootstrapElement } from "./helpers/bootstrap";
-import IndexPage from "./pages";
-import NotFoundPage from "./pages/not-found";
-import SearchPage from "./pages/search";
+
+const IndexPage = lazy(() => import("./pages"));
+const NotFoundPage = lazy(() => import("./pages/not-found"));
+const SearchPage = lazy(() => import("./pages/search"));
 
 const App: Component = () => {
   const queryClient = new QueryClient();
